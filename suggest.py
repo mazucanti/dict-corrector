2913ea5569540642edeb415c4d3f9ff5a0912841
mazucanti <daniel.mazucanti.ismart@gmail.com>
2019-11-24 17:22:58 -0300

    funções de sugestão de escrita de palavras incompletas implementada

--- suggest.py
+++ suggest.py
@@ -1,17 +1,31 @@
 import trees
 
 
-def find_last_letter(word: str):
+def gen_sugg(word: str):
     root = trees.trie_tree()
     node = root
     found = trees.search(node, word)
     if found:
-        return True, None
+        return ""
     if not found:
+        valid_letters = ""
         for letter in word:
             reff = node.copy()
             for child in node.children:
                 if child.letter == letter:
+                    valid_letters += letter
                     node = child
             if node == reff:
-                return False, node
+                return write_sugg(node, valid_letters)
+
+
+def write_sugg(node, word):
+    max_count = 0
+    while not node.word:
+        for child in node.children:
+            if max_count<child.count:
+                max_count = child.count
+                max_node = child
+        word += max_node.letter
+        node = max_node
+    return word
